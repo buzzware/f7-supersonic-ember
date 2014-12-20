1132f705083dbadbe172e9f840b8f549b6af5d3d
Gary McGhee <gary@buzzware.com.au>
2014-12-21 06:56:04 +0800

    Revert "merged in https://github.com/ember-mobile/ember-cli-framework7"
    This reverts commit 8163b630c9bbe58412ba6719491d5a60982c9b99.

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,5 @@
 /* global require, module */
 
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-//var app = new EmberAddon(); //from f7 example
-
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
