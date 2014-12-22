65e3bef7ff243f4ea2881c55997387b232b57346
Gary McGhee <gary@buzzware.com.au>
2014-12-22 09:01:34 +0800

    settings page working now

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,23 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  actions: {
-    itemClicked: function(item) {
-      this.get('f7').alert('Item "'+item+'" clicked');
-    },
-
-    panelLeft: function() {
-      this.get('f7').openPanel('left');
-    },
-
-    refresh: function(deferred) {
-      Ember.run.later(this, function() {
-        deferred.resolve();
-      }, 1000);
-    },
-
-    delete: function(item) {
-      this.removeObject(item);
-    }
-  }
 });
---
+++ app/controllers/index.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    itemClicked: function(item) {
+      this.get('f7').alert('Item "'+item+'" clicked');
+    },
+
+    panelLeft: function() {
+      this.get('f7').openPanel('left');
+    },
+
+    refresh: function(deferred) {
+      Ember.run.later(this, function() {
+        deferred.resolve();
+      }, 1000);
+    },
+
+    delete: function(item) {
+      this.removeObject(item);
+    }
+  }
+});
